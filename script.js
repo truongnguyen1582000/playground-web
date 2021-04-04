cbfcdab486954d951aae71fd302214affeb10aa7
Trương Nguyễn <truongnguyen1582000>
2021-04-04 11:13:01 +0700

    complete

--- script.js
+++ script.js
@@ -2,9 +2,36 @@ const loginBtn = document.querySelector("#login-btn");
 const modal = document.querySelector(".modal");
 
 loginBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+  modal.classList.add("active-modal");
 });
 
+const handleModalClick = () => {
+  modal.classList.remove("active-modal");
+  navMenu.classList.remove("active-menu");
+  hamburger.classList.remove("active");
+};
+
 modal.addEventListener("click", () => {
-  modal.style.display = "none";
+  handleModalClick();
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-menu");
+const navItems = document.querySelectorAll(".navbar-menu-item");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  modal.classList.toggle("active-modal");
+  navMenu.classList.toggle("active-menu");
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    handleModalClick();
+  });
+});
+
+window.addEventListener("scroll", () => {
+  navbar.classList.toggle("navbar-sticky", window.scrollY > 0);
 });
